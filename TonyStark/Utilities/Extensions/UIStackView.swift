9aaacc8aebd598bee962bb2e824f4a5d168664ae
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-15 17:59:48 +0530

    Add extensions on UIView and UIStackView

---
+++ TonyStark/Utilities/Extensions/UIStackView.swift
@@ -0,0 +1,14 @@
+//
+//  UIStackView.swift
+//  TonyStark
+//
+//  Created by Mohammed Sadiq on 15/04/22.
+//
+
+import UIKit
+
+extension UIStackView {
+    static func spacer() -> UIView {
+        return UIView()
+    }
+}
---
+++ TonyStark/Utilities/Extensions/UIView.swift
@@ -0,0 +1,57 @@
+//
+//  UIViewExtensions.swift
+//  TonyStark
+//
+//  Created by Mohammed Sadiq on 14/04/22.
+//
+
+import UIKit
+
+extension UIView {
+    func pin(
+        to view: UIView,
+        padding: UIEdgeInsets? = nil
+    ) {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: padding?.top ?? 0
+            ),
+            self.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: padding?.bottom ?? 0
+            ),
+            self.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: padding?.left ?? 0
+            ),
+            self.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: padding?.right ?? 0
+            )
+        ])
+    }
+    
+    func squareOffConstraint(with side: Double) {
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalToConstant: side),
+            self.heightAnchor.constraint(equalTo: self.widthAnchor)
+        ])
+    }
+    
+    func widthConstraint(with width: Double) {
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalToConstant: width)
+        ])
+    }
+    
+    func heightConstaint(with height: Double) {
+        NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: height)
+        ])
+    }
+    
+    func enableAutolayout() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
