3c2f3fa544b2222ed09114d7ad53731f06a55730
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-17 20:18:54 +0530

    Update TxTableViewCell.swift

--- TonyStark/Controllers/Core/TxTableViewController/TxTableViewCell.swift
+++ TonyStark/Controllers/Core/TxTableViewController/TxTableViewCell.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 class TXTableViewCell: UITableViewCell {
+    override init(
+        style: UITableViewCell.CellStyle,
+        reuseIdentifier: String?
+    ) {
+        super.init(
+            style: style,
+            reuseIdentifier: reuseIdentifier
+        )
+        
+        contentView.isUserInteractionEnabled = false
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
