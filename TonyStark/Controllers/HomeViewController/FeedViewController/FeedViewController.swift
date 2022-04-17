ddb6438c591922b9754d95f27fcc63a3c8bd6d18
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-17 23:59:45 +0530

    Update FeedViewController and ProfileViewController

--- TonyStark/Controllers/HomeViewController/FeedViewController/FeedViewController.swift
+++ TonyStark/Controllers/HomeViewController/FeedViewController/FeedViewController.swift
@@ -7,195 +7,17 @@
 
 import UIKit
 
+
 class FeedViewController: TXTableViewController {
-    private let tweets: [Tweet] = [
-        Tweet(
-            id: "ar93hdk4",
-            text: """
-            English
-            """,
-            creationDate: Date(),
-            meta: TweetMeta(
-                likesCount: 0,
-                commentsCount: 0
-            ),
-            author: User(
-                id: "ar93hdk4",
-                name: "Sadiya Khan",
-                username: "sadiyakhan",
-                image: "https://www.mirchi9.com/wp-content/uploads/2022/02/Mahesh-Fans-Firing-on-Pooja-Hegde.jpg",
-                bio: """
-                I'm simple and soft.
-                """,
-                creationDate: Date(),
-                socialDetails: UserSocialDetails(
-                    followersCount: 0,
-                    followingsCount: 0
-                ),
-                activityDetails: UserActivityDetails(
-                    tweetsCount: 0
-                ),
-                viewables: UserViewables(
-                    follower: false
-                )
-            ),
-            viewables: TweetViewables(
-                liked: true,
-                bookmarked: true
-            )
-        ),
-        Tweet(
-            id: "ar93hdk4",
-            text: """
-            This is rather fun you know.
-            A lot has been said and done about you guys.
-            Let's rock and roll people.
-            """,
-            creationDate: Date(),
-            meta: TweetMeta(
-                likesCount: 0,
-                commentsCount: 0
-            ),
-            author: User(
-                id: "ar93hdk4",
-                name: "Zain Khan",
-                username: "mzaink",
-                image: "https://pbs.twimg.com/profile_images/1483797876522512385/9CcO904A_400x400.jpg",
-                bio: """
-                Hungry for knowledge. Satiated with life. ✌️
-                """,
-                creationDate: Date(),
-                socialDetails: UserSocialDetails(
-                    followersCount: 0,
-                    followingsCount: 0
-                ),
-                activityDetails: UserActivityDetails(
-                    tweetsCount: 0
-                ),
-                viewables: UserViewables(
-                    follower: true
-                )
-            ),
-            viewables: TweetViewables(
-                liked: true,
-                bookmarked: false
-            )
-        ),
-        Tweet(
-            id: "ar93hdk4",
-            text: """
-            I agree it's all opinions here.
-            I need to agree yours.
-            You need to agree mine.
-            Because we stand in two different phase of life and speak.
-            """,
-            creationDate: Date(),
-            meta: TweetMeta(
-                likesCount: 0,
-                commentsCount: 0
-            ),
-            author: User(
-                id: "ar93hdk4",
-                name: "Ramya kembal",
-                username: "RamyaKembal",
-                image: "https://pbs.twimg.com/profile_images/1190200299727851526/A26tGnda_400x400.jpg",
-                bio: """
-                I'm simple and soft.
-                """,
-                creationDate: Date(),
-                socialDetails: UserSocialDetails(
-                    followersCount: 0,
-                    followingsCount: 0
-                ),
-                activityDetails: UserActivityDetails(
-                    tweetsCount: 0
-                ),
-                viewables: UserViewables(
-                    follower: true
-                )
-            ),
-            viewables: TweetViewables(
-                liked: true,
-                bookmarked: true
-            )
-        ),
-        Tweet(
-            id: "ar93hdk4",
-            text: """
-            This is super fun
-            """,
-            creationDate: Date(),
-            meta: TweetMeta(
-                likesCount: 0,
-                commentsCount: 0
-            ),
-            author: User(
-                id: "ar93hdk4",
-                name: "Zain Khan",
-                username: "mzaink",
-                image: "https://pbs.twimg.com/profile_images/1483797876522512385/9CcO904A_400x400.jpg",
-                bio: """
-                I'm simple and soft.
-                """,
-                creationDate: Date(),
-                socialDetails: UserSocialDetails(
-                    followersCount: 0,
-                    followingsCount: 0
-                ),
-                activityDetails: UserActivityDetails(
-                    tweetsCount: 0
-                ),
-                viewables: UserViewables(
-                    follower: true
-                )
-            ),
-            viewables: TweetViewables(
-                liked: true,
-                bookmarked: true
-            )
-        ),
-        Tweet(
-            id: "ar93hdk4",
-            text: """
-            Every entrepreneur I meet says it’s so difficult to find people to work, and on the other hand the unemployment percentage is so high. Something is broken, and it needs fixing asap.
-            """,
-            creationDate: Date(),
-            meta: TweetMeta(
-                likesCount: 0,
-                commentsCount: 0
-            ),
-            author: User(
-                id: "ar93hdk4",
-                name: "Gabbar",
-                username: "GabbbarSingh",
-                image: "https://pbs.twimg.com/profile_images/1271082702326784003/1kIF_loZ_400x400.jpg",
-                bio: """
-                Co-Founder @JoinZorro | Founder @GingerMonkeyIN
-                """,
-                creationDate: Date(),
-                socialDetails: UserSocialDetails(
-                    followersCount: 0,
-                    followingsCount: 0
-                ),
-                activityDetails: UserActivityDetails(
-                    tweetsCount: 0
-                ),
-                viewables: UserViewables(
-                    follower: true
-                )
-            ),
-            viewables: TweetViewables(
-                liked: true,
-                bookmarked: true
-            )
-        ),
-    ]
+    private var state: Result<Paginated<Tweet>, TweetsProviderFailure> = .success(Paginated<Tweet>.empty())
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureNavigationBar()
         configureTableView()
+        
+        populateTableViewWithFeed()
     }
     
     private func configureNavigationBar() {
@@ -228,36 +50,96 @@ class FeedViewController: TXTableViewController {
     }
 }
 
+// MARK: UITableViewDataSource
+extension FeedViewController {
+    private func showActivityIndicator() {
+        DispatchQueue.main.async {
+            [weak self] in
+            let activityIndicator = UIActivityIndicatorView(
+                frame: CGRect(
+                    x: 0,
+                    y: 0,
+                    width: 80,
+                    height: 80
+                )
+            )
+            
+            activityIndicator.startAnimating()
+            
+            self?.tableView.tableFooterView = activityIndicator
+        }
+    }
+    
+    private func hideActivityIndicator() {
+        DispatchQueue.main.async {
+            [weak self] in
+            self?.tableView.tableFooterView = nil
+        }
+    }
+    
+    private func populateTableViewWithFeed() {
+        showActivityIndicator()
+        
+        Task {
+            [weak self] in
+            let result = await TweetsProvider.shared.tweets()
+            
+            self?.hideActivityIndicator()
+            
+            self?.state = result
+            self?.tableView.reloadData()
+        }
+    }
+    
+    
+    override func numberOfSections(
+        in tableView: UITableView
+    ) -> Int {
+        return 1
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        switch state {
+        case .success(let paginated):
+            return paginated.page.count
+        default:
+            return 0
+        }
+    }
+}
 
 // MARK: UITableViewDelegate
 extension FeedViewController {
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: TweetTableViewCell.reuseIdentifier,
-            for: indexPath
-        ) as! TweetTableViewCell
-        
-        cell.populate(with: tweets[indexPath.row])
-        
-        return cell
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        switch state {
+        case .success(result: let paginated):
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TweetTableViewCell.reuseIdentifier,
+                for: indexPath
+            ) as! TweetTableViewCell
+            
+            cell.populate(with: paginated.page[indexPath.row])
+            
+            return cell
+        default:
+            return UITableViewCell()
+        }
     }
     
-    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    override func tableView(
+        _ tableView: UITableView,
+        estimatedHeightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
         return UITableView.automaticDimension
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
-
-// MARK: UITableViewDataSource
-extension FeedViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tweets.count
     }
 }
--- TonyStark/Controllers/HomeViewController/ProfileViewController/ProfileViewController.swift
+++ TonyStark/Controllers/HomeViewController/ProfileViewController/ProfileViewController.swift
@@ -7,50 +7,16 @@
 
 import UIKit
 
-enum ProfileViewSegmentedItem: String {
-    case tweets = "Tweets"
-    case bookmarks = "Bookmarks"
-}
-
 class ProfileViewController: TXTableViewController {
-    private let user = User(
-        id: "ar93hdk4",
-        name: "Zain Khan",
-        username: "mzaink",
-        image: "https://pbs.twimg.com/profile_images/1483797876522512385/9CcO904A_400x400.jpg",
-        bio: """
-        Hungry for knowledge. Satiated with life. ✌️
-        """,
-        creationDate: Date(),
-        socialDetails: UserSocialDetails(
-            followersCount: 0,
-            followingsCount: 0
-        ),
-        activityDetails: UserActivityDetails(
-            tweetsCount: 0
-        ),
-        viewables: UserViewables(follower: true)
-    )
-    
-    let segmentedControl: TXSegmentedControl = {
-        let segmentedControl = TXSegmentedControl(
-            items: [
-                ProfileViewSegmentedItem.tweets.rawValue,
-                ProfileViewSegmentedItem.bookmarks.rawValue
-            ]
-        )
-        
-        segmentedControl.selectedSegmentIndex = 0
-        
-        return segmentedControl
-    }()
+    private var state: Result<User, CurrentUserProviderFailure> = .success(User.empty())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationBar()
         configureTableView()
-        configureSegmentedControl()
+        
+        populate()
     }
     
     private func configureNavigationBar() {
@@ -70,48 +36,67 @@ class ProfileViewController: TXTableViewController {
         )
     }
     
-    private func configureSegmentedControl() {
-        segmentedControl.addTarget(
-            self,
-            action: #selector(onSegmentedControlPressed(_:)),
-            for: .valueChanged
-        )
-    }
-    
-    @objc private func onActionPressed(_ sender: UIBarButtonItem) {
-        print(#function)
-    }
-    
-    @objc private func onSegmentedControlPressed(_ sender: UISegmentedControl) {
+    @objc private func onActionPressed(
+        _ sender: UIBarButtonItem
+    ) {
         print(#function)
     }
 }
 
 // MARK: UITableViewDataSource
 extension ProfileViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    private func populate() {
+        Task {
+            [weak self] in
+            let result = await UserProvider.current.user()
+            
+            self?.state = result
+            self?.tableView.reloadSections(
+                [0],
+                with: .automatic
+            )
+        }
+    }
+    
+    override func numberOfSections(
+        in tableView: UITableView
+    ) -> Int {
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    override func tableView(
+        _ tableView: UITableView,
+        estimatedHeightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
         return UITableView.automaticDimension
     }
 }
 
 // MARK: UITableViewDelegate
 extension ProfileViewController {
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: ProfileTableViewCell.reuseIdentifier,
-            for: indexPath
-        ) as! ProfileTableViewCell
-        
-        cell.populate(with: user)
-        
-        return cell
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        switch state {
+        case .success(let user):
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: ProfileTableViewCell.reuseIdentifier,
+                for: indexPath
+            ) as! ProfileTableViewCell
+            
+            cell.populate(with: user)
+            
+            return cell
+        case .failure(_):
+            return UITableViewCell()
+        }
     }
 }
