a655904770740683709b81f860a54627b5949c50
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-22 02:07:19 +0530

    Create TXEdgeInsets.swift

---
+++ TonyStark/Views/Core/TXEdgeInsets/TXEdgeInsets.swift
@@ -0,0 +1,15 @@
+//
+//  TXEdgeInsets.swift
+//  TonyStark
+//
+//  Created by Mohammed Sadiq on 21/04/22.
+//
+
+import UIKit
+
+struct TXEdgeInsets {
+    let top: Double
+    let right: Double
+    let bottom: Double
+    let left: Double
+}
