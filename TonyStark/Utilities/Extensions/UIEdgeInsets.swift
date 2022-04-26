dee547f904607e933da78e891f1074f4c8b1770b
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-26 16:21:46 +0530

    Update UIEdgeInsets.swift

--- TonyStark/Utilities/Extensions/UIEdgeInsets.swift
+++ TonyStark/Utilities/Extensions/UIEdgeInsets.swift
@@ -18,4 +18,58 @@ extension UIEdgeInsets {
             )
         }
     }
+    
+    static func leading(_ inset: Double) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: 0,
+            left: inset,
+            bottom: 0,
+            right: 0
+        )
+    }
+    
+    static func trailing(_ inset: Double) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: -inset
+        )
+    }
+    
+    static func top(_ inset: Double) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: inset,
+            left: 0,
+            bottom: 0,
+            right: 0
+        )
+    }
+    
+    static func bottom(_ inset: Double) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: 0,
+            left: 0,
+            bottom: -inset,
+            right: 0
+        )
+    }
+    
+    static func horizontallySymmetric(_ inset: Double) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: 0,
+            left: inset,
+            bottom: 0,
+            right: -inset
+        )
+    }
+    
+    static func verticallySymmetric(_ inset: Double) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: inset,
+            left: 0,
+            bottom: -inset,
+            right: 0
+        )
+    }
 }
