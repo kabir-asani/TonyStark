eaa94510557fb4dbb8c84c95914188e0b105ba6f
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-26 10:52:54 +0530

    Add formatter method to Date extension

--- TonyStark/Utilities/Extensions/Date.swift
+++ TonyStark/Utilities/Extensions/Date.swift
@@ -11,4 +11,17 @@ extension Date {
     static func now() -> Date {
         return Date()
     }
+    
+    func formatted(
+        format: String = "h:mm a • d/MM/yyyy"
+    ) -> String {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.dateFormat = format
+        
+        let formattedDate = dateFormatter.string(from: self)
+        
+        return formattedDate
+    }
 }
