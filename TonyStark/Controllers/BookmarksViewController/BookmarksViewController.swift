41d9030f74100f29e2e28dad92ec907c58889ab3
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-27 15:55:57 +0530

    Create BookmarksViewController

---
+++ TonyStark/Controllers/BookmarksViewController/BookmarksViewController.swift
@@ -0,0 +1,134 @@
+//
+//  BookmarksViewController.swift
+//  TonyStark
+//
+//  Created by Mohammed Sadiq on 27/04/22.
+//
+
+import UIKit
+
+class BookmarksViewController: TXViewController {
+    private var state: Result<Paginated<Tweet>, BookmarksProvider.BookmarksFailure> = .success(.default())
+    
+    // Declare
+    private let tableView: TXTableView = {
+        let tableView = TXTableView()
+        
+        tableView.enableAutolayout()
+        
+        return tableView
+    }()
+    
+    // Configure
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addSubviews()
+        
+        configureNavigationBar()
+        configureTableView()
+        
+        populateTableView()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = "Bookmarks"
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.tableHeaderView = TXView(frame: .zero)
+        
+        tableView.register(
+            PartialTweetTableViewCell.self,
+            forCellReuseIdentifier: PartialTweetTableViewCell.reuseIdentifier
+        )
+        
+        tableView.pin(to: view)
+    }
+    
+    // Populate
+    
+    // Interact
+}
+
+
+// MARK: TXTableViewDataSource
+extension BookmarksViewController: TXTableViewDataSource {
+    func populateTableView() {
+        Task {
+            [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            let result = await BookmarksProvider.shared.bookmarks()
+            
+            strongSelf.state = result
+            strongSelf.tableView.reloadData()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        switch state {
+        case .success(let paginated):
+            return paginated.page.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        switch state {
+        case .success(let paginated):
+            let cell = tableView.dequeueReusableCellWithIndexPath(
+                withIdentifier: PartialTweetTableViewCell.reuseIdentifier,
+                for: indexPath
+            ) as! PartialTweetTableViewCell
+            
+            let tweet = paginated.page[indexPath.row]
+            
+            cell.configure(withTweet: tweet)
+            
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+}
+
+// MARK: TXTableViewDelegate
+extension BookmarksViewController: TXTableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        willDisplay cell: UITableViewCell,
+        forRowAt indexPath: IndexPath
+    ) {
+        switch state {
+        case .success(let paginated):
+            if indexPath.row  == paginated.page.count - 1 {
+                cell.separatorInset = .empty
+            } else {
+                cell.separatorInset = .leading(20)
+            }
+        default:
+            break
+        }
+    }
+}
