373192f8a4446eb022d0e33a1873544edbb241da
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-04-27 14:40:32 +0530

    Create LikesViewController

---
+++ TonyStark/Controllers/LikesViewController/LikesViewController.swift
@@ -0,0 +1,142 @@
+//
+//  LikesViewController.swift
+//  TonyStark
+//
+//  Created by Mohammed Sadiq on 27/04/22.
+//
+
+import UIKit
+
+class LikesViewController: TXViewController {
+    private var tweet: Tweet!
+    
+    // Declare
+    private var state: Result<Paginated<User>, LikesProvider.LikesFailure> = .success(.default())
+    
+    let tableView: TXTableView = {
+        let tableView = TXTableView()
+        
+        tableView.enableAutolayout()
+        
+        return tableView
+    }()
+    
+    // Configure
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addSubviews()
+        
+        configureNavigationBar()
+        configureTableView()
+        
+        populateTableView()
+    }
+    
+    func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    func configureNavigationBar() {
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableHeaderView = TXView(frame: .zero)
+        
+        tableView.register(
+            PartialUserTableViewCell.self,
+            forCellReuseIdentifier: PartialUserTableViewCell.reuseIdentifier
+        )
+        
+        tableView.pin(to: view)
+    }
+    
+    // Populate
+    func populate(withTweet tweet: Tweet) {
+        self.tweet = tweet
+    }
+    
+    // Interact
+}
+
+// MARK: TXTableViewDataSource
+extension LikesViewController: TXTableViewDataSource {
+    private func populateTableView() {
+        Task {
+            [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            let result = await LikesProvider.shared.likes()
+            
+            strongSelf.state = result
+            strongSelf.tableView.reloadData()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        switch state {
+        case .success(let paginated):
+            return paginated.page.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        switch state {
+        case .success(let paginated):
+            let cell = tableView.dequeueReusableCellWithIndexPath(
+                withIdentifier: PartialUserTableViewCell.reuseIdentifier,
+                for: indexPath
+            ) as! PartialUserTableViewCell
+            
+            cell.configure(withUser: paginated.page[indexPath.row])
+            
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+}
+
+// MARK: TXTableViewDelegate
+extension LikesViewController: TXTableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        willDisplay cell: UITableViewCell,
+        forRowAt indexPath: IndexPath
+    ) {
+        switch state {
+        case .success(let paginated):
+            if indexPath.row  == paginated.page.count - 1 {
+                cell.separatorInset = .empty
+            } else {
+                cell.separatorInset = .leading(20)
+            }
+        default:
+            break
+        }
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        estimatedHeightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        return TXTableView.automaticDimension
+    }
+}
