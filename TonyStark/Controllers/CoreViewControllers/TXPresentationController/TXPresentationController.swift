e7ba8053829e242b7cb4289030a02714c92e0674
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-05 13:23:15 +0530

    Create TXPresentationController (subclass of UIPresentationController)

---
+++ TonyStark/Controllers/CoreViewControllers/TXPresentationController/TXPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  TXPresentationViewController.swift
+//  TonyStark
+//
+//  Created by Mohammed Sadiq on 05/05/22.
+//
+
+import UIKit
+
+
+class TXPresentationController: UIPresentationController {
+    
+}
