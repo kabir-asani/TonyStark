893b941a3bbccfb0e0b91fe20f439e39bc4c89ac
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-06 08:41:28 +0530

    Refactor TXLocalStorageAssistant

---
+++ TonyStark/Utilities/TXLocalStorageAssistant/TXExtensiveLocalStorageAssistant.swift
@@ -0,0 +1,45 @@
+//
+//  TXExtensiveLocalStorageAssistant.swift
+//  TonyStark
+//
+//  Created by Mohammed Sadiq on 06/05/22.
+//
+
+import Foundation
+
+// TODO: Implement functionality across all TXLocalStorageAssistantProtocol's methods
+class TXExtensiveLocalStorageAssistant: TXLocalStorageAssistantProtocol {
+    func boot() async {
+        // TODO: Implement this to create persistent storage container
+    }
+    
+    func exists(key: String) async -> Bool {
+        return false
+    }
+    
+    func store<T: Codable>(
+        key: String,
+        value: T
+    ) async throws -> TXLocalStorageElement<T> {
+        throw TXStoreFailure.unknown
+    }
+    
+    func retrieve<T: Codable>(
+        key: String
+    ) async throws -> TXLocalStorageElement<T> {
+        throw TXRetrieveFailure.unknown
+    }
+    
+    func update<T: Codable>(
+        key: String,
+        value: T
+    ) async throws -> TXLocalStorageElement<T> {
+        throw TXUpdateFailure.unknown
+    }
+    
+    func delete<T: Codable>(
+        key: String
+    ) async throws -> TXLocalStorageElement<T> {
+        throw TXDeleteFailure.unknown
+    }
+}
