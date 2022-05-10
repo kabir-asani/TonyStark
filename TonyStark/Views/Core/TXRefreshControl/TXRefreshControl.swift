3d5cbb7093833c0f4950cf7ff83cab3e745f5a06
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-10 15:28:11 +0530

    Update TXRefreshControl.swift

--- TonyStark/Views/Core/TXRefreshControl/TXRefreshControl.swift
+++ TonyStark/Views/Core/TXRefreshControl/TXRefreshControl.swift
@@ -7,6 +7,38 @@
 
 import UIKit
 
+protocol TXRefreshControlDelegate: AnyObject {
+    func refreshControlDidChange(_ control: TXRefreshControl)
+}
+
 class TXRefreshControl: UIRefreshControl {
+    weak var delegate: TXRefreshControlDelegate?
     
+    override init() {
+        super.init()
+        
+        addTarget(
+            self,
+            action: #selector(onRefreshControllerChanged(_:)),
+            for: .valueChanged
+        )
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addTarget(
+            self,
+            action: #selector(onRefreshControllerChanged(_:)),
+            for: .valueChanged
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func onRefreshControllerChanged(_ refreshControl: TXRefreshControl) {
+        delegate?.refreshControlDidChange(self)
+    }
 }
