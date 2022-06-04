93dd906649d51f253028142d80714a2e51ded755
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-06-04 14:51:47 +0530

    Add parse method to DateX

--- TonyStark/Utilities/TXExtensions/DateX.swift
+++ TonyStark/Utilities/TXExtensions/DateX.swift
@@ -8,17 +8,36 @@
 import Foundation
 
 extension Date {
+    enum DateFormat: String {
+        // E.g. "2022-06-03T07:58:54Z"
+        case utc = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        
+        // E.g. 12:30 PM • 12/12/2012
+        case visiblyPleasing = "h:mm a • d/MM/yyyy"
+    }
+    
     static func now() -> Date {
         return Date()
     }
     
+    static func parse(string dateString: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = DateFormat.utc.rawValue
+        let date = dateFormatter.date(from: dateString)
+        
+        return date ?? .now()
+    }
+    
     func formatted(
-        format: String = "h:mm a • d/MM/yyyy"
+        as format: DateFormat = .utc,
+        withTimeZone timeZone: TimeZone = .current
     ) -> String {
         let dateFormatter = DateFormatter()
         
-        dateFormatter.locale = Locale(identifier: "en_US")
-        dateFormatter.dateFormat = format
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = timeZone
+        dateFormatter.dateFormat = format.rawValue
         
         let formattedDate = dateFormatter.string(from: self)
         
