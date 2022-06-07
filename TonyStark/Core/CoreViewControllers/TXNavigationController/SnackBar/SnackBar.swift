01575eb5c0592e639152ac0b4d90f2b718e1a734
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-06-08 01:16:20 +0530

    Refactor SnackBar.swift

--- TonyStark/Core/CoreViewControllers/TXNavigationController/SnackBar/SnackBar.swift
+++ TonyStark/Core/CoreViewControllers/TXNavigationController/SnackBar/SnackBar.swift
@@ -17,22 +17,9 @@ class SnackBar: TXVisualEffectView {
         case warning
     }
     
-    static func show(
-        text: String,
-        variant: Variant = .informative,
-        duration: TimeInterval = 2
-    ) {
-        let snackBar = SnackBar(
-            text: text,
-            variant: variant,
-            duration: duration
-        )
-        
-        snackBar.present()
-    }
-    
     private let variant: Variant
     private let duration: TimeInterval
+    private let dismissible: Bool
     
     private let image: TXImageView = {
         let image = TXImageView()
@@ -59,13 +46,15 @@ class SnackBar: TXVisualEffectView {
     }()
     
     // Arrange
-    private init(
+    init(
         text: String,
-        variant: Variant = .informative,
-        duration: TimeInterval = 2
+        variant: Variant,
+        duration: TimeInterval,
+        dismissible: Bool
     ) {
         self.variant = variant
         self.duration = duration
+        self.dismissible = dismissible
         
         super.init(effect: UIBlurEffect(style: .prominent))
         
@@ -151,130 +140,11 @@ class SnackBar: TXVisualEffectView {
         let velocity = sender.velocity(in: self)
         
         if velocity.y < 0 {
-            dismissSnackBar()
-        }
-    }
-}
-
-extension SnackBar {
-    func present() {
-        dismissPreviousSnackBar {
-            [weak self] in
-            guard let strongSelf = self else {
-                return
-            }
-            
-            if let keyWindow = AppDelegate.keyWindow {
-                keyWindow.addSubview(strongSelf)
-                
-                strongSelf.pin(
-                    toLeftOf: keyWindow,
-                    withInset: 16,
-                    byBeingSafeAreaAware: true
+            if dismissible {
+                TXEventBroker.shared.emit(
+                    event: HideSnackBarEvent()
                 )
-                strongSelf.pin(
-                    toRightOf: keyWindow,
-                    withInset: 16,
-                    byBeingSafeAreaAware: true
-                )
-                strongSelf.pin(
-                    toTopOf: keyWindow,
-                    byBeingSafeAreaAware: false
-                )
-                
-                let hiddenSnackbarTransform = CGAffineTransform(
-                    translationX: 0,
-                    y: strongSelf.frame.size.height - keyWindow.safeAreaInsets.top
-                )
-                
-                strongSelf.layer.setAffineTransform(hiddenSnackbarTransform)
-                
-                UIView.animate(
-                    withDuration: 0.1,
-                    delay: 0,
-                    options: .curveLinear
-                ) {
-                    [weak self] in
-                    guard let strongSelf = self else {
-                        return
-                    }
-                    
-                    let visibleSnackbarTransform = CGAffineTransform(
-                        translationX: 0,
-                        y: keyWindow.safeAreaInsets.top + 16
-                    )
-                    
-                    strongSelf.layer.setAffineTransform(visibleSnackbarTransform)
-                } completion: {
-                    [weak self] completed in
-                    guard let strongSelf = self else {
-                        return
-                    }
-                    
-                    if completed {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + strongSelf.duration) {
-                            [weak self] in
-                            guard let strongSelf = self else {
-                                return
-                            }
-                            
-                            strongSelf.dismissSnackBar()
-                        }
-                    }
-                }
             }
-        }
-    }
-    
-    func dismissSnackBar() {
-        UIView.animate(
-            withDuration: 0.1,
-            delay: 0,
-            options: .curveLinear
-        ) {
-            [weak self] in
-            guard let strongSelf = self else {
-                return
-            }
-            
-            let hiddenSnackbarTransform = CGAffineTransform(
-                translationX: 0,
-                y: -strongSelf.frame.size.height
-            )
-            
-            strongSelf.layer.setAffineTransform(hiddenSnackbarTransform)
-        } completion: {
-            [weak self] completed in
-            guard let strongSelf = self else {
-                return
-            }
-            
-            if completed {
-                strongSelf.removeFromSuperview()
-            }
-        }
-    }
-    
-    private func dismissPreviousSnackBar(completion: (() -> Void)? = nil) {
-        if let keyWindow = AppDelegate.keyWindow {
-            let mayBeSnackBar = keyWindow.subviews.first {
-                view in
-                
-                if view is SnackBar {
-                    return true
-                }
-                
-                return false
-            }
-            
-            if let snackBar = mayBeSnackBar as? SnackBar {
-                snackBar.dismissSnackBar()
-                completion?()
-            } else {
-                completion?()
-            }
-        } else {
-            completion?()
         }
     }
 }
