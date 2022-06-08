98c21777be543bb70d33b32ca514a25727397929
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-06-08 22:47:05 +0530

    Update FeedDataStore

--- TonyStark/DataStrores/FeedDataStore/FeedDataStore.swift
+++ TonyStark/DataStrores/FeedDataStore/FeedDataStore.swift
@@ -10,12 +10,37 @@ import Foundation
 class FeedDataStore: DataStore {
     static let shared = FeedDataStore()
     
+    private static let feedURL = "\(FeedDataStore.baseUrl)/timeline"
+    
     private override init() { }
     
-   
     func feed(
         after nextToken: String? = nil
     ) async -> Result<Paginated<Tweet>, FeedFailure> {
-        return .failure(.unknown)
+        if let session = CurrentUserDataStore.shared.session {
+            do {
+                let feedResult = try await TXNetworkAssistant.shared.get(
+                    url: Self.feedURL,
+                    headers: secureHeaders(
+                        withAccessToken: session.accessToken
+                    )
+                )
+                
+                if feedResult.statusCode == 200 {
+                    let feed = try TXJsonAssistant.decode(
+                        SuccessData<Paginated<Tweet>>.self,
+                        from: feedResult.data
+                    ).data
+                    
+                    return .success(feed)
+                } else {
+                    return .failure(.unknown)
+                }
+            } catch {
+                return .failure(.unknown)
+            }
+        } else {
+            return .failure(.unknown)
+        }
     }
 }
